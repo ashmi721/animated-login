5221834c95ceb510897c0b8230ed8c04b7247a17
ashmi721 <ashmitak721@gmail.com>
2023-01-13 10:51:25 +0545

    animation

---
+++ style.php
@@ -0,0 +1,11 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+Your account has been sucessfully created <?php echo ($_POST)["login"];?><br>
+Conform your account
+
+</head>
